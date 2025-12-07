35d0610087803726884ffc14fa9aa9158db824ba
Majid.mhdp <Majid.mhdp@gmail.com>
2025-12-07 23:51:45 +0800

    Component lifecycle

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject, DestroyRef } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -7,6 +7,31 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css',
 })
-export class ServerStatusComponent {
-  currentStatus = 'online';
+export class ServerStatusComponent implements OnInit {
+  currentStatus: 'online' | 'offline' | 'unknown' = 'offline';
+  // private timer?: ReturnType<typeof setInterval>;
+  private destoyRef = inject(DestroyRef);
+  constructor() {}
+
+  ngOnInit() {
+    const timer = setInterval(() => {
+      const rnd = Math.random();
+
+      if (rnd < 0.5) {
+        this.currentStatus = 'online';
+      } else if (rnd < 0.9) {
+        this.currentStatus = 'offline';
+      } else {
+        this.currentStatus = 'unknown';
+      }
+    }, 5000);
+
+    this.destoyRef.onDestroy(() => {
+      clearInterval(timer);
+    });
+  }
+
+  // ngOnDestroy() {
+  //   clearInterval(this.timer);
+  // }
 }
--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,13 +1,16 @@
 import { Component } from '@angular/core';
-import { ControlComponent } from "../../../shared/control/control.component";
+import { ControlComponent } from '../../../shared/control/control.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-ticket',
   standalone: true,
-  imports: [ControlComponent],
+  imports: [ControlComponent, FormsModule],
   templateUrl: './new-ticket.component.html',
-  styleUrl: './new-ticket.component.css'
+  styleUrl: './new-ticket.component.css',
 })
 export class NewTicketComponent {
-
+  onSubmit() {
+    console.log('Form submitted');
+  }
 }
